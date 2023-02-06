0b4769eee2b1b72f3a10718050115f1fbbbe4e69
Sahil Gupta <87527245+Sahilgupta4103@users.noreply.github.com>
2023-02-06 21:45:01 +0530

    initial stage

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'UTF-8'
+DISSCONECT_MSG = 'disconnect'
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER,PORT)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    
+string = input("enter the message \n")
+send(string)
+ 
+
+
+    
